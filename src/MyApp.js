dacab433eeec295ee476a329f298a3ef6dce6f3e
seeddhee <73972398+seeddhee@users.noreply.github.com>
2023-04-16 18:03:31 -0700

    Linked

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,23 +1,55 @@
 import Table from './Table'
-import React, {useState} from 'react';
 import Form from './form'
-
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-
+  
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:8000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
+ }
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
         return i !== index
       });
       setCharacters(updated);
     }
-    function updateList(person) {
-      setCharacters([...characters, person]);
-    }
-    function updateList(person) {
-      setCharacters([...characters, person]);
-    }
+  
+    function updateList(person) { 
+      makePostCall(person).then( result => {
+      if (result && result.status === 200)
+         setCharacters([...characters, person] );
+      });
+   }
+
+
+    async function makePostCall(person){
+      try {
+         const response = await axios.post('http://localhost:8000/users', person);
+         return response;
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+   }
+
+    useEffect(() => { //called when MyApp is mounted the data is fetched from backend once only when it is mounted
+      fetchAll().then( result => {
+         if (result)
+            setCharacters(result);
+       });
+    }, [] );
+
     return (
         <div className="container">
            <Table characterData={characters} removeCharacter={removeOneCharacter} />
@@ -26,6 +58,11 @@ function MyApp() {
       )
      
 }
+
+// used to perform side effects in your components
+
+
+
 
 
 export default MyApp;
